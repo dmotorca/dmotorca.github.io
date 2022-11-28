6bf1e8baafe96232d3724bb3f9415c8da7e5624d
David <dmotorca@pdx.edu>
2022-11-27 22:22:21 -0800

    Added so more details to the javascript code and the form submission

--- index.js
+++ index.js
@@ -3,19 +3,26 @@ let form = document.querySelector('form');
 form.onsubmit = (event) => {
   event.preventDefault();
 
-  const submission = getFormData();
+  const submission = getFormData(event);
 };
 
-getFormData = () => {
+getFormData = (event) => {
   let submission = {};
+
   submission.name = form.elements.contactName.value;
   submission.email = form.elements.contactEmail.value;
   submission.message = form.elements.contactMessage.value;
+  submission.number = form.elements.contactNumber.value;
+  submission.time = '${event.timeStamp}';
 
   if (submission.name == '') {
     alert('Name must be filled out');
   } else if (submission.email == '') {
     alert('Email must be filled out');
+  } else if (submission.number == '') {
+    alert('Number must be filled out');
+  } else if (submission.message == '') {
+    alert('Contact Me must be filled out');
   }
 
   return submission;
